c42a3cf9f2f269d8dfab8cfb901d71f9b92f33e2
Nathan Brooks <natebrooks004@gmail.com>
2021-01-03 00:39:06 -0800

    2016/day1 tests b

--- tests/year2016/day1.py
+++ tests/year2016/day1.py
@@ -1,2 +1,23 @@
 # -*- coding: utf-8 -*-
+""""""
 
+
+from unittest import TestCase, main
+
+
+from aoc.year2016.day1 import a, b
+from aoc.utils import get_input
+
+
+class TestA(TestCase):
+    def test_input(self):
+        self.assertEqual(ab(get_input(2016, 1)), 161)
+
+
+class TestB(TestCase):
+    def test_input(self):
+        self.assertEqual(ab(get_input(2016, 1)), 110)
+
+
+if __name__ == '__main__':
+    main()
