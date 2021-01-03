e0d292a6636a6b369e5f6b9ed3290b723239161a
Nathan Brooks <natebrooks004@gmail.com>
2021-01-02 16:47:09 -0800

    2015/day3

---
+++ aoc/2015/day3.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Perfectly Spherical Houses in a Vacuum
+"""
+
+
+from itertools import accumulate, islice
+
+
+DIRS = {'^':1j, '>':1, 'v':-1j, '<':-1}
+
+
+def a(text):
+    return len(set({0} | set(accumulate(map(DIRS.get, text)))))
+
+
+def b(text):
+    locations = {0}
+    locations.update(accumulate(map(DIRS.get, islice(text, 0, None, 2))))
+    locations.update(accumulate(map(DIRS.get, islice(text, 1, None, 2))))
+    
+    return len(locations)
+
+
+if __name__ == '__main__':
+    text = '^<<<>vv><><>v^><>^^^^vvvv'
+    
+    print(a(text))
+    print(b(text))
