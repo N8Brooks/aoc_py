88842ccb61d23982b162b54107b2923da59125cb
Nathan Brooks <natebrooks004@gmail.com>
2021-01-03 00:10:43 -0800

    update 2015/day3 tests

--- tests/year2015/day3.py
+++ tests/year2015/day3.py
@@ -1,2 +1,23 @@
 # -*- coding: utf-8 -*-
+""""""
 
+
+from unittest import TestCase, main
+
+
+from aoc.year2015.day3 import a, b
+from aoc.utils import get_input
+
+
+class TestA(TestCase):
+    def test_input(self):
+        self.assertEqual(a(get_input(2015, 3)), 2081)
+
+
+class TestB(TestCase):
+    def test_input(self):
+        self.assertEqual(b(get_input(2015, 3)), 2341)
+
+
+if __name__ == '__main__':
+    main()
