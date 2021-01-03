373d2f8adfe59e2d233f92e2a909eb5b47787000
Nathan Brooks <natebrooks004@gmail.com>
2021-01-02 16:18:21 -0800

    2015/day2

---
+++ aoc/2015/day2.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+I Was Told There Would Be No Math
+"""
+
+
+from itertools import starmap
+
+
+def process(text):
+    return tuple(sorted(map(int, line.split('x'))) for line in text.split())
+
+
+def a(text):
+    def paper(x, y, z):
+        return 3 * x * y + 2 * (x * z + y * z)
+    
+    return sum(starmap(paper, process(text)))
+
+
+def b(text):
+    def ribbon(x, y, z):
+        return x + x + y + y + x * y * z
+    
+    return sum(starmap(ribbon, process(text)))
+
+
+if __name__ == '__main__':
+    text = '3x4x5'
+    print(a(text))
+    print(b(text))
