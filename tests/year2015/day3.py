ff38bd6f9c2a6d1aef530ee0d0ae011768f369dc
Nathan Brooks <natebrooks004@gmail.com>
2021-01-03 09:58:10 -0800

    add examples to 2015/day3 tests

--- tests/year2015/day3.py
+++ tests/year2015/day3.py
@@ -12,11 +12,29 @@ from aoc.utils import get_input
 class TestA(TestCase):
     def test_input(self):
         self.assertEqual(a(get_input(2015, 3)), 2081)
+    
+    def test_example_1(self):
+        self.assertEqual(a('>'), 2)
+    
+    def test_example_2(self):
+        self.assertEqual(a('^>v<'), 4)
+    
+    def test_example_3(self):
+        self.assertEqual(a('^v^v^v^v^v'), 2)
 
 
 class TestB(TestCase):
     def test_input(self):
         self.assertEqual(b(get_input(2015, 3)), 2341)
+    
+    def test_example_1(self):
+        self.assertEqual(b('^v'), 3)
+    
+    def test_example_2(self):
+        self.assertEqual(b('^>v<'), 3)
+    
+    def test_example_3(self):
+        self.assertEqual(b('^v^v^v^v^v'), 11)
 
 
 if __name__ == '__main__':
