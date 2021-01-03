124487367c046ba0ab593cb179567157b74268a6
Nathan Brooks <natebrooks004@gmail.com>
2021-01-03 09:49:06 -0800

    add example tests for 2015/day4

--- tests/year2015/day4.py
+++ tests/year2015/day4.py
@@ -9,13 +9,17 @@ from aoc.year2015.day4 import ab
 from aoc.utils import get_input
 
 
-class TestA(TestCase):
-    def test_input(self):
+class TestAB(TestCase):
+    def test_input_a(self):
         self.assertEqual(ab(get_input(2015, 4), '00000'), 346386)
-
-
-class TestB(TestCase):
-    def test_input(self):
+    
+    def test_example_1(self):
+        self.assertEqual(ab('abcdef', '00000'), 609043)
+    
+    def test_example_2(self):
+        self.assertEqual(ab('pqrstuv', '00000'), 1048970)
+    
+    def test_input_b(self):
         self.assertEqual(ab(get_input(2015, 4), '000000'), 9958218)
 
 
