b102f9be4b2ce667bd91cda9cd1aba16eeed7b55
Nathan Brooks <natebrooks004@gmail.com>
2021-01-02 17:18:55 -0800

    2017/day1

---
+++ aoc/2017/day1.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+https://adventofcode.com/2017/day/1
+"""
+
+
+def a(text):
+    def match(i):
+        return text[i] == text[i - 1]
+    
+    return sum(int(text[i]) for i in filter(match, range(len(text))))
+
+
+def b(text):
+    def match(i, halfway=len(text) // 2):
+        return text[i] == text[i - halfway]
+    
+    return sum(int(text[i]) for i in filter(match, range(len(text))))
+
+
+if __name__ == '__main__':
+    print(a(text))
+    print(b(text))
