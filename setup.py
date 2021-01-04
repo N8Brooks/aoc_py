1277cd158f0b2453f36f831af4ec2faad1388295
Nathan Brooks <natebrooks004@gmail.com>
2021-01-03 18:30:37 -0800

    fix incorrect stuff in example setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,24 @@
 import setuptools
 
+
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
+
 setuptools.setup(
-    name="aoc", # Replace with your own username
+    name="aoc",  # Replace with your own username
     version="0.0.1",
-    author="Example Author",
-    author_email="author@example.com",
-    description="A small example package",
+    author="Nathan Brooks",
+    author_email="natebrooks004@gmail.com",
+    description="Advent of Code Solutions",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/N8Brooks/aoc",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.9',
+    python_requires=">=3.9",
+    install_requires=["numpy>=1.19.2", "more_itertools>=8.6.0"],
 )
