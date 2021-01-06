e403cfb5d5f8db45e27c3ae7994d50445d43496e
Nathan Brooks <natebrooks004@gmail.com>
2021-01-06 15:39:11 -0800

    2017/3

---
+++ aoc/year2017/day3.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+https://adventofcode.com/2017/day/3
+"""
+
+
+from math import ceil, sqrt
+from operator import add
+from itertools import takewhile, repeat
+
+from iteration_utilities import applyfunc
+
+from data.utils import get_input
+
+
+NEIGHBORS = (-1 + 0j, 1 + 0j, -1 + 1j, 0 + 1j, 1 + 1j, -1 - 1j, 0 - 1j, 1 - 1j)
+
+
+def part1(text):
+    n = int(text) + 1
+    layer = ceil(sqrt(n) / 2 - 0.5)
+    return layer + abs(((n - 1) % (2 * layer) - layer)) - 1
+
+
+def part2(text):
+    def move(c):
+        if 0 == c.real == c.imag:
+            return 1
+        elif -c.real < c.imag:
+            return c + 1j if c.imag < c.real else c - 1
+        else:
+            return c - 1j if c.real < c.imag else c + 1
+
+    n = int(text)
+    grid = {(c := 0): 1}
+
+    for c in takewhile(lambda _: grid[c] <= n, applyfunc(move, c)):
+        grid[c] = sum(grid.get(x, 0) for x in map(add, repeat(c), NEIGHBORS))
+
+    return grid[c]
+
+
+if __name__ == "__main__":
+    text = get_input(2017, 3)
+
+    print(part1(text))
+    print(part2(text))
---
+++ tests/year2017/test_day3.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+https://adventofcode.com/2017/day/3
+"""
+
+
+from unittest import main, TestCase
+
+from aoc.year2017.day3 import part1, part2
+from data.utils import get_input
+
+
+class TestPart1(TestCase):
+    def test_input(self):
+        self.assertEqual(part1(get_input(2017, 3)), 480)
+
+    def test_example_1(self):
+        self.assertEqual(part1(1), 0)
+        
+    def test_example_2(self):
+        self.assertEqual(part1(12), 3)
+        
+    def test_example_3(self):
+        self.assertEqual(part1(23), 2)
+        
+    def test_example_4(self):
+        self.assertEqual(part1(1024), 31)
+
+
+class TestPart2(TestCase):
+    def test_input(self):
+        self.assertEqual(part2(get_input(2017, 3)), 349975)
+
+    def test_example_1(self):
+        self.assertEqual(part2(0), 1)
+        
+    def test_example_2(self):
+        self.assertEqual(part2(54), 57)
+        
+    def test_example_3(self):
+        self.assertEqual(part2(800), 806)
+        
+    def test_example_4(self):
+        self.assertEqual(part2(1000000), 1009457)
+
+
+if __name__ == "__main__":
+    main()
