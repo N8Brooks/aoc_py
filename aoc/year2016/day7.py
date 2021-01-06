233b812f6c8cebe9cb487e982f6f11e4c4525fab
Nathan Brooks <natebrooks004@gmail.com>
2021-01-05 18:13:13 -0800

    2016/7

---
+++ aoc/year2016/day7.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+https://adventofcode.com/2016/day/7
+"""
+
+
+from itertools import starmap
+
+from more_itertools import windowed, flatten
+
+from aoc.utils import get_input
+
+
+def process(ip):
+    def sort(part):
+        *hyp, ext = part.split("]")
+        return hyp, ext
+
+    hyps, exts = zip(*map(sort, ip.split("[")))
+
+    return flatten(hyps), exts
+
+
+def part1(text):
+    def abba(a, b, c, d):
+        return a == d and b == c and a != b
+
+    def abbas(ip):
+        return any(starmap(abba, windowed(ip, 4)))
+
+    def tls(hyps, exts):
+        return not any(map(abbas, hyps)) and any(map(abbas, exts))
+
+    return sum(starmap(tls, map(process, text.split())))
+
+
+def part2(text):
+    def aba(abc):
+        return abc[0] == abc[2] and abc[0] != abc[1]
+
+    def abas(ip):
+        return filter(aba, windowed(ip, 3))
+
+    def rev(a, b, _):
+        return (b, a, b)
+
+    def ssl(hyps, exts):
+        inside = set(flatten(map(abas, hyps)))
+        return not inside.isdisjoint(starmap(rev, flatten(map(abas, exts))))
+
+    return sum(starmap(ssl, map(process, text.strip().split())))
+
+
+if __name__ == "__main__":
+    text = get_input(2016, 7)
+
+    print(part1(text))
+    print(part2(text))
---
+++ tests/year2016/test_day7.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+"""
+https://adventofcode.com/2016/day/7
+"""
+
+
+from unittest import main, TestCase
+
+from aoc.year2016.day7 import part1, part2
+from aoc.utils import get_input
+
+
+EXAMPLE_1 = """
+abba[mnop]qrst
+abcd[bddb]xyyx
+aaaa[qwer]tyui
+ioxxoj[asdfgh]zxcvbn
+"""
+
+EXAMPLE_2 = """
+aba[bab]xyz
+xyx[xyx]xyx
+aaa[kek]eke
+zazbz[bzb]cdb
+"""
+
+
+class TestA(TestCase):
+    def test_input(self):
+        self.assertEqual(part1(get_input(2016, 7)), 115)
+
+    def test_example_1(self):
+        self.assertEqual(part1(EXAMPLE_1), 2)
+
+    def test_example_2(self):
+        self.assertEqual(part1(EXAMPLE_2), 0)
+
+
+class TestB(TestCase):
+    def test_input(self):
+        self.assertEqual(part2(get_input(2016, 7)), 231)
+
+    def test_example_1(self):
+        self.assertEqual(part2(EXAMPLE_1), 0)
+
+    def test_example_2(self):
+        self.assertEqual(part2(EXAMPLE_2), 3)
+
+
+if __name__ == "__main__":
+    main()
