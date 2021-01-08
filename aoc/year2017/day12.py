0a4769bc63f30dd29c6f32691e7199bc8f198f20
Nathan Brooks <natebrooks004@gmail.com>
2021-01-07 22:06:24 -0800

    2017/12

---
+++ aoc/year2017/day12.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+https://adventofcode.com/2017/day/12
+"""
+
+
+from functools import partial
+from itertools import repeat
+from operator import eq
+
+from iteration_utilities import count_items
+
+from data.utils import get_input
+
+
+def unite(text):
+    def find(a):
+        return a if a == parent.setdefault(a, a) else find(parent[a])
+
+    parent = {}
+
+    for line in text.strip().split("\n"):
+        a, bs = line.split(" <-> ")
+        parent.update(zip(map(find, bs.split(", ")), repeat(find(a))))
+
+    return parent, find
+
+
+def part1(text, group="0"):
+    parent, find = unite(text)
+
+    return count_items(map(find, parent.values()), partial(eq, find(group)))
+
+
+def part2(text):
+    parent, find = unite(text)
+
+    return len(set(map(find, parent.values())))
+
+
+if __name__ == "__main__":  # pragma: no cover
+    text = get_input(2017, 12)
+
+    print(part1(text))
+    print(part2(text))
---
+++ tests/year2017/test_day12.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+https://adventofcode.com/2017/day/12
+"""
+
+
+from unittest import main, TestCase
+
+from aoc.year2017.day12 import part1, part2
+from data.utils import get_input
+
+
+EXAMPLE = """0 <-> 2
+1 <-> 1
+2 <-> 0, 3, 4
+3 <-> 2, 4
+4 <-> 2, 3, 6
+5 <-> 6
+6 <-> 4, 5
+"""
+
+
+class TestPart1(TestCase):
+    def test_input(self):
+        self.assertEqual(part1(get_input(2017, 12)), 306)
+
+    def test_example(self):
+        self.assertEqual(part1(EXAMPLE), 6)
+
+
+class TestPart2(TestCase):
+    def test_input(self):
+        self.assertEqual(part2(get_input(2017, 12)), 200)
+
+    def test_example(self):
+        self.assertEqual(part2(EXAMPLE), 2)
+
+
+if __name__ == "__main__":  # pragma: no cover
+    main()
