f620dc040990b560a8139c92ff2503a26b4d2b7b
Nathan Brooks <natebrooks004@gmail.com>
2021-01-09 14:23:31 -0800

    2015/23

---
+++ aoc/year2015/day23.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+https://adventofcode.com/2015/day/23
+"""
+
+
+from functools import partial
+from itertools import takewhile
+from operator import gt
+
+from iteration_utilities import applyfunc, consume
+
+from data.utils import get_input
+
+
+def compute(text, a, b, target):
+    def value(reg):
+        return registers[reg] if reg.isalpha() else int(reg)
+
+    def process(line):
+        instruction, *operands = line.replace(",", "").split()
+        if instruction == "jmp":
+            return instruction, None, int(operands[0]) - 1
+        elif len(operands) == 1:
+            return instruction, operands[0], None
+        else:
+            return instruction, operands[0], int(operands[1]) - 1
+
+    def execute(pointer):
+        instruction, register, offset = program[pointer]
+
+        if instruction == "hlf":
+            registers[register] //= 2
+        elif instruction == "tpl":
+            registers[register] *= 3
+        elif instruction == "inc":
+            registers[register] += 1
+        elif instruction == "jmp":
+            pointer += offset
+        elif instruction == "jie" and registers[register] % 2 == 0:
+            pointer += offset
+        elif instruction == "jio" and registers[register] == 1:
+            pointer += offset
+
+        return pointer + 1
+
+    registers = {"a": a, "b": b}
+    program = tuple(map(process, text.strip().split("\n")))
+
+    consume(takewhile(partial(gt, len(program)), applyfunc(execute, 0)), None)
+
+    return registers[target]
+
+
+def part1(text, a=0, b=0, target="b"):
+    return compute(text, a, b, target)
+
+
+def part2(text, a=1, b=0, target="b"):
+    return compute(text, a, b, target)
+
+
+if __name__ == "__main__":  # pragma: no cover
+    text = get_input(2015, 23)
+
+    print(part1(text))
+    print(part2(text))
---
+++ tests/year2015/test_day23.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+https://adventofcode.com/2015/day/23
+"""
+
+
+from unittest import main, TestCase
+
+from aoc.year2015.day23 import part1, part2
+from data.utils import get_input
+
+
+EXAMPLE = """
+inc a
+jio a, +2
+tpl a
+inc a
+"""
+
+
+class TestPart1(TestCase):
+    def test_input(self):
+        self.assertEqual(part1(get_input(2015, 23)), 307)
+
+    def test_example_a(self):
+        self.assertEqual(part1(EXAMPLE, target="a"), 2)
+
+    def test_example_b(self):
+        self.assertEqual(part1(EXAMPLE, target="b"), 0)
+
+
+class TestPart2(TestCase):
+    def test_input(self):
+        self.assertEqual(part2(get_input(2015, 23)), 160)
+
+    def test_example_a(self):
+        self.assertEqual(part2(EXAMPLE, a=2, target="a"), 10)
+
+    def test_example_b(self):
+        self.assertEqual(part2(EXAMPLE, target="b"), 0)
+
+
+if __name__ == "__main__":  # pragma: no cover
+    main()
