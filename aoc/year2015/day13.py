5ff9e7058ec25c68cbe77d7f7905963e95802e3a
Nathan Brooks <natebrooks004@gmail.com>
2021-01-18 21:29:46 -0800

    2015/13

--- aoc/year2015/day13.py
+++ aoc/year2015/day13.py
@@ -12,38 +12,39 @@ import re
 from data.utils import get_input
 
 
-class Family:
+def family(text, buffer=False):
+    @cache
+    def happiness(i, s):
+        def add(j):
+            return pairs[(1 << i) | (1 << j)] + happiness(j, s | (1 << j))
+
+        if s == (1 << n) - 1:
+            return pairs[1 | (1 << i)]
+
+        return max(add(j) for j in range(n) if not (s & (1 << j)))
+
     r = re.compile(r"(\w+) would (\w+) (\d+) happiness units by sitting next to (\w+).")
 
-    def __init__(self, text, buffer=False):
-        self.individuals = {}
-        self.pairs = defaultdict(int)
+    individuals = {}
+    pairs = defaultdict(int)
 
-        for line in text.splitlines():
-            a, sign, val, b = Family.r.match(line).groups()
-            a = self.individuals.setdefault(a, len(self.individuals))
-            b = self.individuals.setdefault(b, len(self.individuals))
-            self.pairs[(1 << a) | (1 << b)] += int(val) if sign == "gain" else -int(val)
+    for line in text.splitlines():
+        a, sign, val, b = r.match(line).groups()
+        a = individuals.setdefault(a, len(individuals))
+        b = individuals.setdefault(b, len(individuals))
+        pairs[(1 << a) | (1 << b)] += int(val) if sign == "gain" else -int(val)
 
-        self.n = len(self.individuals) + buffer
+    n = len(individuals) + buffer
 
-    @cache
-    def happiness(self, i=0, s=1):
-        def add(j):
-            return self.pairs[(1 << i) | (1 << j)] + self.happiness(j, s | (1 << j))
-
-        if s == (1 << self.n) - 1:
-            return self.pairs[1 | (1 << i)]
-
-        return max(add(j) for j in range(self.n) if not (s & (1 << j)))
+    return happiness(0, 1)
 
 
 def part1(text):
-    return Family(text).happiness()
+    return family(text)
 
 
 def part2(text):
-    return Family(text, True).happiness()
+    return family(text, True)
 
 
 if __name__ == "__main__":  # pragma: no cover
